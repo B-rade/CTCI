e6752147dedd1244cd0af83dac94ec78694288a0
B-rade <B-rade@users.noreply.github.com>
2018-07-05 13:18:31 -0500

    added more unit tests for 5_1.py

--- chapter_5/5_1.py
+++ chapter_5/5_1.py
@@ -15,12 +15,34 @@ def insert_binary_integers(N: int, M: int, i: int, j: int) -> int:
 
 
 class TestOneAway(unittest.TestCase):
-    def test_true1(self):
+    def test_equal1(self):
         N = 0b1000000000
         M = 0b10011
         i = 2
         j = 6
         answer = 0b1001001100
+        self.assertEqual(insert_binary_integers(N, M, i, j), answer)
+
+    def test_equal2(self):
+        N = 0b1111111111
+        M = 0b10011
+        i = 2
+        j = 6
+        answer = 0b1111001111
+        self.assertEqual(insert_binary_integers(N, M, i, j), answer)
+
+    def test_equal3(self):
+        """
+        If j-i+1 is greater than length of M, it doesn't shift answer to start at jth bit, but rather
+        terminates at ith bit
+        >>insert_binary_integers(0b1111111111, 0b10011, 2, 8)
+        0b1001001111
+        """
+        N = 0b1111111111
+        M = 0b10011
+        i = 2
+        j = 8
+        answer = 0b1001001111
         self.assertEqual(insert_binary_integers(N, M, i, j), answer)
 
 
